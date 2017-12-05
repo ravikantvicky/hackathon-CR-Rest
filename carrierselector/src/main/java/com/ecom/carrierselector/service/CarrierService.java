5051c69d96d136c684bfb49d16d31568c493a366
Ravi Kant Pandey <ravikantvicky@gmail.com>
2017-12-06 01:44:06 +0530

    2nd Service

--- carrierselector/src/main/java/com/ecom/carrierselector/service/CarrierService.java
+++
@@ -1,9 +0,0 @@
-package com.ecom.carrierselector.service;
-
-import java.util.List;
-
-import com.ecom.carrierselector.domain.CarrierDetails;
-
-public interface CarrierService {
-	public List<CarrierDetails> getAllCarriers();
-}
